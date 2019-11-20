7a0740398092b37d309a8e832e70c67ecaee1cbe
dada <davaa4510@gmail.com>
2019-11-20 12:39:39 +0800

    unuudur

--- app/Http/Controllers/companyController.php
+++ app/Http/Controllers/companyController.php
@@ -20,7 +20,6 @@ class companyController extends Controller
 
     public function getCompanyToNew(){
         $companies = DB::table('tb_companies')->get();
-        $companies1 = DB::table('tb_companies')->get();
         return DataTables::of($companies)
             ->make(true);
     }
