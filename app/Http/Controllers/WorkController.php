a140eeabeef5ddb82e735ef0f50370b292296d58
Zulaa <zulaa_0722@yahoo.com>
2020-04-03 14:59:25 +0800

    change buttonclick, companyview

--- app/Http/Controllers/WorkController.php
+++ app/Http/Controllers/WorkController.php
@@ -4,8 +4,20 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Http\Requests;
+use App\Work;
+use DB;
+use Yajra\DataTables\DataTables;
+use Redirect;
 
 class WorkController extends Controller
 {
     //
+    public static function getCompactWorks($worktype)
+    {
+      $works = DB::table('tb_work')
+          ->where("work_type_id", "=", $worktype)
+          ->get();
+      return $works;
+    }
 }
--- public/js/work_type/hideShowWorks.js
+++ public/js/work_type/hideShowWorks.js
@@ -7,3 +7,74 @@ $(document).on('click', '[type=checkbox]', function(){
         $("#worktypeid" + id).hide();
     }
 });
+
+// $(document).on('click', '[type=button]', function(){
+//     var id = $(this).attr("btnworkid");
+//     alert("aa"+id);
+// });
+
+$(document).ready(function () {
+    $('button[class^="btnWorkTypeID"]').click(function () {
+        var id = $(this).attr("btnworkid");
+        alert("aa"+id);
+    });
+});
+
+
+
+
+// $(document).ready(function(){
+//     $("#btnWork").click(function(e){
+//         e.preventDefault();
+//         var id = $(this).attr("btnworkid");
+//
+//         alert("aa"+id);
+
+        // var isInsert = true;
+        // if($("#txtEditCompanyName").val()==""||$("#txtEditCompanyName").val()==null){
+        //     alertify.error("Аж ахуйн нэгжийн нэр оруулаагүй байна!!!");
+        //     isInsert = false;
+        // }
+        // if($("#cmbEditHeseg").val()=="0"){
+        //     alertify.error("Хэсэг ээ сонгоно уу!!!");
+        //     isInsert = false;
+        // }
+        // if($("#txtEditAjliinHeseg").val()==""||$("#txtEditAjliinHeseg").val()==null){
+        //     alertify.error("Ажлын хэсэг оруулаагүй байна!!!");
+        //     isInsert = false;
+        // }
+        // if($("#txtEditGereeOgnoo").val()==""||$("#txtEditGereeOgnoo").val()==null){
+        //     alertify.error("Ажил эхэлсэн огноо оруулаагүй байна!!!");
+        //     isInsert = false;
+        // }
+        // if($("#txtEditHunHuch").val()==""||$("#txtEditHunHuch").val()==null){
+        //     alertify.error("Хүн хүч оруулаагүй байна!!!");
+        //     isInsert = false;
+        // }
+        // if($("#txtEditMashinTehnik").val()==""||$("#txtEditMashinTehnik").val()==null){
+        //     alertify.error("Ажлын машин техник оруулаагүй байна!!!");
+        //     isInsert = false;
+        // }
+        // if(isInsert == false){return;}
+
+
+
+        // $.ajax({
+        //   type: 'POST',
+        //   url: newWorksUrl,
+        //   data: $("#saveWorks"+id).serialize(),
+        //   success:function(response){
+        //       alertify.alert(response);
+        //       refresh();
+        //   },
+        //   error: function(jqXhr, json, errorThrown){// this are default for ajax errors
+        //     var errors = jqXhr.responseJSON;
+        //     var errorsHtml = '';
+        //     $.each(errors['errors'], function (index, value) {
+        //         errorsHtml += '<ul class="list-group"><li class="list-group-item alert alert-danger">' + value + '</li></ul>';
+        //     });
+        //     alert(errorsHtml);
+        //   }
+        // });
+//     });
+// });
